ed376b8ca5b0c3bc1867f18b9c6fa501ebfc8441
SatoShun <shun.sato1@gmail.com>
2018-03-30 06:45:33 +0000

    use ktlint

--- app/src/main/java/com/github/satoshun/example/ci/MainActivity.kt
+++ app/src/main/java/com/github/satoshun/example/ci/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.github.satoshun.example.ci
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main_act)
